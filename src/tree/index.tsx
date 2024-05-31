303eef6263eb30dbb90e2fca385caa0a2bdc7f93
shrim0092 <shrim0092@gmail.com>
2024-05-30 20:21:08 -0400

    Part 2: Next, with data - Bonus 1 - Inverse Tree

--- src/tree/index.tsx
+++ src/tree/index.tsx
@@ -4,8 +4,8 @@ import animalTree from "./data.json";
 const renderTree = (node, depth) => (
   <div className="line" key={node.name} 
   style={{ marginLeft: `${depth * 40}px` }}>
-    {node.name}
     {node.children && node.children.map(child => renderTree(child, 1))}
+    {node.name}
   </div>
 );
 
