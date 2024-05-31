70a95b8df50d230ddee22503beda250e6d24fc52
shrim0092 <shrim0092@gmail.com>
2024-05-30 20:34:26 -0400

    Part 4: And persistence

--- src/tree/index.tsx
+++ src/tree/index.tsx
@@ -30,18 +30,81 @@ const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, depth: number, 
 const Tree = () => {
   const [tree, setTree] = useState(animalTree);
   const [lastChild, setLastChild] = useState({})
+  const [response, setResponse] = useState([]);
 
+  // let req = new XMLHttpRequest();
 
-  useEffect(() => {
+  // req.onreadystatechange = () => {
+  //   if (req.readyState == XMLHttpRequest.DONE) {
+
+  //     let jsonResponse = JSON.parse(req.response);
+  //     setResponse(jsonResponse.record);
+  //     console.log(jsonResponse.record)
+  //   }
+  // };
+  
+  // req.open("GET", "https://api.jsonbin.io/v3/b/6658c89ce41b4d34e4fc0857/latest", true);
+  // req.setRequestHeader("X-Master-Key", "$2a$10$8kKvSPuePtQSA/VM/nIbu.FEd4pLZDOcXxh4m5QM/q1szHkycAbBe");
+  // req.send();
+
+  const postData = (tree) => {
+    let url = "https://api.jsonbin.io/v3/b/665911f9acd3cb34a85056c8";
+    let key = "$2a$10$8kKvSPuePtQSA/VM/nIbu.FEd4pLZDOcXxh4m5QM/q1szHkycAbBe";
+    let accessKey = "$2a$10$7IlFOebe4Hfq37U.AasdduXcSbkY5ZGmD.Ip4KFg6TvQbQEa5e1LS";
+
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        'X-Master-Key': key,
+        'Content-type' : 'application/json',
+        'X-Access-Key': accessKey
+      },  
+      body: JSON.stringify(tree)
+    })
+    .then(response => response.json())
+    .then(response => {
+      console.log("PUT Success response")
+      console.log(response)})
+    .catch(err => console.log(err))
+  }
+
+  const fetchData = async () => {
+    let url = "https://api.jsonbin.io/v3/b/665911f9acd3cb34a85056c8/latest";
+    let key = "$2a$10$8kKvSPuePtQSA/VM/nIbu.FEd4pLZDOcXxh4m5QM/q1szHkycAbBe";
+    
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'X-Master-Key': key
+      },
+    })
+    .then(response => response.json())
+    .then(response => {
+      console.log("Fetch success")
+      console.log(response.record)
+      setResponse(response.record);
+      setTree(response.record);
+    })
+    .catch(err => console.log(err))
+  }
+
+   useEffect(() => {
+    fetchData();
+
     let levelmap = {};
     processlastchild(tree, 0, levelmap);
     setLastChild(levelmap);
     console.log(levelmap);
+
   }, [])
 
   const updateTree = (newWord: any, depth: number) => {
     let currentLevel = tree;
+    console.log(currentLevel);
+
+  
     for (let i = 0; i < depth; i++) {
+      if(!currentLevel.children || currentLevel.children.length == 0) break;
       currentLevel = currentLevel.children[currentLevel.children.length - 1];
     }
     if (!currentLevel.children) {
@@ -49,6 +112,7 @@ const Tree = () => {
     }
     currentLevel.children.push({ name: newWord });
     setTree({ ...tree });
+    postData(tree)
   };
 
   const removeNode = (nodeToRemove: { name: string; children?: undefined; } | { name: string; children: ({ name: string; children?: undefined; } | { name: string; children: { name: string; }[]; })[]; }, depth: number) => {
@@ -62,11 +126,13 @@ const Tree = () => {
     }
     currentLevel.children = currentLevel.children.filter(node => node !== nodeToRemove);
     setTree({ ...tree });
+    postData(tree)
   };
 
   return (
 
     <div className="tree">
+      {console.log(tree)}
       {renderTreeWithCrossMarks(tree, 0, updateTree, removeNode, true, lastChild)}
     </div>
   );
