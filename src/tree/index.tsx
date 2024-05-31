23301696c3f89c5ce2535b43445be175a0985d9e
shrim0092 <shrim0092@gmail.com>
2024-05-30 20:23:48 -0400

    Part 2: Next, with data - Bonus 2 - Number each line

--- src/tree/index.tsx
+++ src/tree/index.tsx
@@ -1,18 +1,27 @@
 import "./index.css";
 import animalTree from "./data.json";
 
-const renderTree = (node, depth) => (
+const formatName = (name: string, dots: number) => {
+  let res = "";
+  res += name[0];
+  for(let i = 0; i < dots; i++) res += ".";
+  for(let i = 1; i < name.length; i++) res += name[i];
+  return res;
+}
+
+const renderTree = (node, depth, level) => (
   <div className="line" key={node.name} 
   style={{ marginLeft: `${depth * 40}px` }}>
-    {node.children && node.children.map(child => renderTree(child, 1))}
-    {node.name}
+    {formatName(node.name, level)}
+    {node.children && node.children.map(child => renderTree(child, 1, level + 1))}
+    {/* {node.name} for bonus*/}
   </div>
 );
 
 const Tree = () => {
   return (
     <div className="tree">
-      { renderTree(animalTree, 0) }
+      { renderTree(animalTree, 0, 0) }
       </div>
   );
 };
